a7c39a0f14ab0cd8f9fb1486cb2d1eae2f6a2184
GENYF <gnyf0915@gmail.com>
2021-02-24 16:27:21 +0900

    Delete Visual Studio File

--- Source Code/Safrowave/Release/Wire/utility/twi.c.d
+++
@@ -1,7 +0,0 @@
-C:\Users\gnyf0\AppData\Local\Temp\VMBuilds\Safrowave\micro\Release\Wire\utility\twi.c.o: \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src\utility\twi.c \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\micro/pins_arduino.h \
- c:\program\ files\ (x86)\arduino\hardware\arduino\avr\variants\leonardo\pins_arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src\utility\twi.h
